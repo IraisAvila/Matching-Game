1965c6b4ce5fd230be0487764d1edfdfa2f20536
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-06-29 17:37:26 -0500

    set up Topics

--- Matching-Game/matching-game/src/App.js
+++ Matching-Game/matching-game/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'animate.css';
+import Homepage from './Components/HomePage';
 
 function App() {
+const array = ["Topic 1","Topic 2","Topic 3"]
+const listTopics = array.map(function(item) {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <Homepage Topic={item}/>
+  )
+})
+  return (
+    <div>
+      <div>
+        <h1 className="animate__animated animate__rubberBand">Matching</h1>
+      </div>
+      {listTopics}
     </div>
   );
 }
---
+++ Matching-Game/matching-game/src/Components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = (params) => {
+  return (
+    <div>
+    <div className="square">
+    </div>
+    <p>{params.Topic}</p>
+    </div>
+  );
+  
+};
+export default Homepage;
