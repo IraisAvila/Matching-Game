80a38d1273f4209d9e2a53d74ded3791418d7470
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-07-02 22:32:42 -0500

    First Game fully functional

--- Matching-Game/matching-game/src/Components/FirstGame.js
+++ Matching-Game/matching-game/src/Components/FirstGame.js
@@ -1,91 +1,100 @@
 import React from "react";
-import '../App.css'
+import "../App.css";
 import { useState } from "react";
 import SingleCard from "./SingleCard";
 import { useEffect } from "react";
 
 const cardImages = [
-  { "src": "/images/batman-1.png", matched: false },
-  { "src": "/images/superman-1.png", matched: false },
-  { "src": "/images/hulk-1.png", matched: false },
-  { "src": "/images/flash-1.png", matched: false },
-  { "src": "/images/thor-1.png", matched: false },
-  { "src": "/images/captainamerica-1.png", matched: false },
-  { "src": "/images/spiderman-1.png", matched: false },
-  { "src": "/images/ironman-1.png", matched: false },
-  { "src": "/images/wonderwoman-1.png", matched: false },
-  { "src": "/images/aquaman-1.png", matched: false }
-]
- function FirstGame() {
-   const [cards, setCards] = useState([])
-   const [choiceOne, setChoiceOne] = useState(null)
-   const [choiceTwo, setChoiceTwo] = useState(null)
+  { src: "/images/batman-1.png", matched: false },
+  { src: "/images/superman-1.png", matched: false },
+  { src: "/images/hulk-1.png", matched: false },
+  { src: "/images/flash-1.png", matched: false },
+  { src: "/images/thor-1.png", matched: false },
+  { src: "/images/captainamerica-1.png", matched: false },
+  { src: "/images/spiderman-1.png", matched: false },
+  { src: "/images/ironman-1.png", matched: false },
+  { src: "/images/wonderwoman-1.png", matched: false },
+  { src: "/images/aquaman-1.png", matched: false },
+];
+function FirstGame() {
+  const [cards, setCards] = useState([]);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
-   //Shuffle cards
-   const shuffleCards = () => {
-     //Spread Syntax:It expands the array into individual elements. So, it can be used to expand the array in a places where zero or more elements are expected. 
-     const shuffledCards = [ ...cardImages, ...cardImages]
-     //.sort:The sort() overwrites the original array. Sorts the elements as strings in alphabetical and ascending order.
-     .sort(() => Math.random() -0.5)
-     //.map:creates a new array from calling a function for every array element. 
-     .map((card) => ({...card, id: Math.random() }))
+  //Shuffle cards
+  const shuffleCards = () => {
+    //Spread Syntax:It expands the array into individual elements. So, it can be used to expand the array in a places where zero or more elements are expected.
+    const shuffledCards = [...cardImages, ...cardImages]
+      //.sort:The sort() overwrites the original array. Sorts the elements as strings in alphabetical and ascending order.
+      .sort(() => Math.random() - 0.5)
+      //.map:creates a new array from calling a function for every array element.
+      .map((card) => ({ ...card, id: Math.random() }));
 
-     setCards(shuffledCards)
-   }
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards);
+  };
 
-   //Handle a choice
-   const handleChoice = (card) => {
-     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-   }
+  //Handle a choice
+  const handleChoice = (card) => {
+    // Stop the user from being able to click the first card twice
+    if(card.id === choiceOne?.id) return;
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
-   //Compare 2 selected cards
-   useEffect(() => {
-     if (choiceOne && choiceTwo) {
-
+  //Compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
-        setCards(prevCards => {
-          return prevCards.map(card => {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
             if (card.src === choiceOne.src) {
-              return{...card, matched: true}
+              return { ...card, matched: true };
             } else {
-              return card
+              return card;
             }
-          })
-        })
-        resetTurn()
+          });
+        });
+        resetTurn();
       } else {
-        resetTurn()
+        setTimeout(() => resetTurn(), 1200);
       }
-     }
-   }, [choiceOne, choiceTwo])
+    }
+  }, [choiceOne, choiceTwo]);
 
-   console.log(cards)
+  console.log(cards);
 
-// Reset choices
-const resetTurn = () => {
-  setChoiceOne(null)
-  setChoiceTwo(null)
+  // Reset choices
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setDisabled(false);
+  };
+  // Start Game Automatically
+  useEffect(() => {
+    shuffleCards()
+
+  }, [])
+  return (
+    <div className="Hero">
+      <h2>Matching Game 1</h2>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-   return (
-     <div className="Hero">
-   <h2>Matching Game 1</h2>
-   <button onClick={shuffleCards}>Start</button>
-   
-   <div className ="card-grid">
-     {cards.map(card => (
-       <SingleCard 
-       key={card.id} 
-       card={card} 
-       handleChoice={handleChoice}
-       flipped={card === choiceOne || card === choiceTwo || card.matched}
-       />
 
-     ))}
-   </div>
-   </div>
-   );
-
- }
-
- export default FirstGame;
-
+export default FirstGame;
